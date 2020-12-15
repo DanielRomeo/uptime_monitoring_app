e4a150633ee9d9f7867be907b494ac01318480ba
Daniel Mamphekgo <Danielromeo99@gmail.com>
2020-12-15 13:05:07 +0200

    Able to return path and request method

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 // create a http server and make it listen on port 5000:
-const http = require('http');
-const server = http.createServer((request, response)=>{
-	response.end('Hello world \n');
+const http = require("http");
+const url = require('url');
+
+const server = http.createServer((request, response) => {
+	response.end("Hello world \n");
+
+	/*we're passing ture in order to tell it to call the "query string" module itself*/
+	const parsedUrl = url.parse(request.url, true);
+	let path = parsedUrl.pathname;
+	let trimmedPath = path.replace(/^\/+|\/+$/g, ''); // trims away all slashes in the url
+
+	let method = request.method.toLowerCase();
+	console.log(`Recieved the path: ${trimmedPath} with the method: ${method}`);
 });
 
 // start the server and tell it on which port it should listen on:
 const PORT = 5000;
-server.listen(PORT, ()=>{
+server.listen(PORT, () => {
 	console.log(`Server is listening on post ${PORT}`);
 });
